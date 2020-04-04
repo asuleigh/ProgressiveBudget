893c74caf935b30c79b14a14b7d4950898167fb0
Amanda Surber <surberaleigh@gmail.com>
2020-04-04 08:13:18 -0400

    trying to fix app

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-<link rel="manifest" href="manifest.json"></link>
+
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker.register("/serviceWorker.js").then(reg => {
