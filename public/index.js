925fe59ac9c69980b5b6fbec76861268fb523611
Amanda Surber <surberaleigh@gmail.com>
2020-04-04 08:21:29 -0400

    trying to fix serviceworker

--- public/index.js
+++ public/index.js
@@ -1,11 +1,3 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("serviceWorker.js").then(reg => {
-        console.log("We found your service worker file!", reg);
-      });
-    });
-  }
-
 let transactions = [];
 let myChart;
 
